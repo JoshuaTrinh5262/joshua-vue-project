4a29fc53310e1b716fc3ccad6403c19f6ae3c825
Trịnh Quang Trường <103560993+JoshuaTrinh5262@users.noreply.github.com>
2023-12-23 18:10:17 +0700

    Update Table component 2

--- src/Layout/Components/TableComponent.vue
+++ src/Layout/Components/TableComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <table class="table table-sm ">
+    <table class="table table-sm">
       <thead>
         <tr>
           <th v-for="field in fields" :key="field.key" :id="field.key">{{ field.value }}</th>
@@ -8,11 +8,10 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, key) in items" :key="key">
-          <td>{{ item.id }}</td>
-          <td>{{ item.source_text }}</td>
-          <td>{{ item.target_text }}</td>
-          <td>{{ item.created_date }}</td>
+        <tr v-for="(item, index) in items" :key="index">
+          <td v-for="field in fields" :key="field.key">
+            {{ item[field.key] }}
+          </td>
           <td>
             <button type="button" class="btn btn-sm btn-success" @click="updateRow(item.id)"><i class="pe-7s-file"></i></button>
             <button type="button" class="btn btn-sm btn-warning" @click="deleteRow(item.id)"><i class="pe-7s-trash"></i></button>
