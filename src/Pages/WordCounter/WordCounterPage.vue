d55f99f4ef91bf7b37304633ec4c07af262222f8
Trịnh Quang Trường <103560993+JoshuaTrinh5262@users.noreply.github.com>
2023-12-23 11:15:58 +0700

    update WordCounterPage

--- src/Pages/WordCounter/WordCounterPage.vue
+++ src/Pages/WordCounter/WordCounterPage.vue
@@ -1,19 +1,17 @@
 <template>
     <div>
-        <page-title 
-            :heading=heading
-            :subheading=subheading
-            :icon=icon></page-title>
-        <div>
+        <page-title-component :heading=heading :subheading=subheading :icon=icon></page-title-component>
+
+        <div class="content">
             <textarea v-model="text" @input="updateCounters"></textarea>
-            <div>
+            <div class="counter">
                 <p>Words: {{ words }}</p>
                 <p>Sentences: {{ sentences }}</p>
                 <p>Paragraphs: {{ paragraphs }}</p>
                 <p>Total Unique Word: {{ uniqueWordCount }}</p>
             </div>
         </div>
-        <div>
+        <div class="word-frequency">
             <h3>Word Frequency:</h3>
             <ul>
                 <li v-for="item in wordFrequency" :key="item.word">
@@ -24,79 +22,79 @@
     </div>
 </template>
 <script>
-import PageTitle from "../../Layout/Components/PageTitle";
+    import PageTitleComponent from "../../Layout/Components/PageTitleComponent.vue";
 
-export default { 
-    name: "WordCounterPage",
+    export default { 
+        name: "WordCounterPage",
 
-    components: {
-        PageTitle
-    },
-
-    data() {
-        return {
-            heading: 'Word Counter',
-            subheading: 'easy-to-use tool for tracking word and character counts in your writing.',
-            icon: 'pe-7s-phone icon-gradient bg-premium-dark',
-            text: '',
-            words: 0,
-            sentences: 0,
-            paragraphs: 0,
-            uniqueWordCount: 0,
-            wordFrequency: [],
-            sortEnabled: false,
-        };
-    },
-
-    methods: {
-        updateCounters() {
-            this.wordCounter();
-            this.sentenceCounter();
-            this.paragraphCounter();
-            this.wordFrequencyCounter();
+        components: {
+            PageTitleComponent
         },
 
-        wordCounter() {
-            const wordCount = this.text ? this.text.match(/\b\w+('\w+)?\b/g) : [];
-            this.words = wordCount.length;
+        data() {
+            return {
+                heading: 'Word Counter',
+                subheading: 'easy-to-use tool for tracking word and character counts in your writing.',
+                icon: 'pe-7s-phone icon-gradient bg-premium-dark',
+                text: '',
+                words: 0,
+                sentences: 0,
+                paragraphs: 0,
+                uniqueWordCount: 0,
+                wordFrequency: [],
+                sortEnabled: false,
+            };
         },
 
-        sentenceCounter() {
-            const sentenceCount = this.text ? this.text.split(/[.?!][ "\n]/) : [];
-            this.sentences = sentenceCount.length > 0 ? sentenceCount.length - 1 : 0;
+        methods: {
+            updateCounters() {
+                this.wordCounter();
+                this.sentenceCounter();
+                this.paragraphCounter();
+                this.wordFrequencyCounter();
+            },
+
+            wordCounter() {
+                const wordCount = this.text ? this.text.match(/\b\w+('\w+)?\b/g) : [];
+                this.words = wordCount.length;
+            },
+
+            sentenceCounter() {
+                const sentenceCount = this.text ? this.text.split(/[.?!][ "\n]/) : [];
+                this.sentences = sentenceCount.length > 0 ? sentenceCount.length - 1 : 0;
+            },
+
+            paragraphCounter() {
+                const paragraphCount = this.text ? this.text.split(/\n/) : [];
+                this.paragraphs = paragraphCount.length;
+            },
+
+            wordFrequencyCounter() {
+                //count
+                const wordArray = this.text ? this.text.match(/\b\w+('\w+)?\b/g) : [];
+                // Calculate word frequencies
+                const wordFrequency = wordArray.reduce((acc, word) => {
+                    const existingWord = acc.find(item => item.word === word.toLowerCase());
+
+                    if (existingWord) {
+                        existingWord.count += 1;
+                    } else {
+                        acc.push({ word, count: 1 });
+                    }
+
+                    return acc;
+                }, []);
+
+                // Sort the array based on word in alphabetical order
+                wordFrequency.sort((a, b) => a.word.localeCompare(b.word));
+
+                // Update the state with the sorted word frequency array
+                this.wordFrequency = wordFrequency;
+                this.uniqueWordCount = wordFrequency.length;
+
+            },
         },
-
-        paragraphCounter() {
-            const paragraphCount = this.text ? this.text.split(/\n/) : [];
-            this.paragraphs = paragraphCount.length;
-        },
-
-        wordFrequencyCounter() {
-            //count
-            const wordArray = this.text ? this.text.match(/\b\w+('\w+)?\b/g) : [];
-            // Calculate word frequencies
-            const wordFrequency = wordArray.reduce((acc, word) => {
-                const existingWord = acc.find(item => item.word === word.toLowerCase());
-
-                if (existingWord) {
-                    existingWord.count += 1;
-                } else {
-                    acc.push({ word, count: 1 });
-                }
-
-                return acc;
-            }, []);
-
-            // Sort the array based on word in alphabetical order
-            wordFrequency.sort((a, b) => a.word.localeCompare(b.word));
-
-            // Update the state with the sorted word frequency array
-            this.wordFrequency = wordFrequency;
-            this.uniqueWordCount = wordFrequency.length;
-
-        },
-    },
-};
+    };
 </script>
 
 <style scoped>
