07d91f15d85d1423cf047b6a4e85781bcef2cfad
Joshua Trịnh <joshuatrinh5262@gmail.com>
2024-08-22 23:10:40 +0700

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,246 +1,388 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+// import {supabase} from "@/supabase/supabase"
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     scrollBehavior() {
         return window.scrollTo({ top: 0, behavior: 'smooth' });
     },
     mode: 'history',
     routes: [
-
-        // Dashboards
-
         {
-            path: '/',
-            name: 'analytics',
+            path: '/admin',
+            name: 'admin',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Dashboards/AnalyticsComponent.vue'),
         },
         {
-            path: '/word-counter',
+            path: '/admin/dashboard',
+            name: 'dashboard',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
+            component: () => import('../DemoPages/Dashboards/AnalyticsComponent.vue'),
+        },
+        // Dashboards
+        {
+            path: '/admin',
+            name: 'analytics',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
+            component: () => import('../DemoPages/Dashboards/AnalyticsComponent.vue'),
+        },
+        {
+            path: '/admin/word-counter',
             name: 'word-counter',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/WordCounter/WordCounterPage.vue'),
         },
         {
-            path: '/calculator',
+            path: '/admin/calculator',
             name: 'calculator',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/CalculatorPage.vue'),
         },
-        // Pages
-
-        {
-            path: '/pages/login-boxed',
-            name: 'login-boxed',
-            meta: {layout: 'userpages'},
-            component: () => import('../DemoPages/UserPages/LoginBoxed.vue'),
-        },
-        {
-            path: '/pages/register-boxed',
-            name: 'register-boxed',
-            meta: {layout: 'userpages'},
-            component: () => import('../DemoPages/UserPages/RegisterBoxed.vue'),
-        },
-        {
-            path: '/pages/forgot-password-boxed',
-            name: 'forgot-password-boxed',
-            meta: {layout: 'userpages'},
-            component: () => import('../DemoPages/UserPages/ForgotPasswordBoxed.vue'),
-        },
-
         // DataSet
         {
-            path: '/users',
+            path: '/admin/users',
             name: 'users',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/Datasets/UserPage.vue'),
         },
         {
-            path: '/dataset',
+            path: '/admin/dataset',
             name: 'dataset',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/Datasets/DatasetPage.vue'),
         },
         {
-            path: '/translations',
+            path: '/admin/translations',
             name: 'translations',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/Datasets/LanguaguePage.vue'),
         },
 
-        // Vtuber Data
+        // Vtubers Data
         {
-            path: '/talents',
+            path: '/admin/talents',
             name: 'talents',
-            component: () => import('../Pages/Datasets/UserPage.vue'),
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
+            component: () => import('../Pages/Vtubers/TalentsPage.vue'),
         },
         {
-            path: '/discographies',
-            name: 'discographies',
-            component: () => import('../Pages/Datasets/DatasetPage.vue'),
-        },
-        {
-            path: '/galleries',
-            name: 'galleries',
-            component: () => import('../Pages/Datasets/LanguaguePage.vue'),
-        },
-        {
-            path: '/agencies',
+            path: '/admin/agencies',
             name: 'agencies',
-            component: () => import('../Pages/Datasets/LanguaguePage.vue'),
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
+            component: () => import('../Pages/Vtubers/AgenciesPage.vue'),
         },
         {
-            path: '/events',
-            name: 'events',
-            component: () => import('../Pages/Datasets/LanguaguePage.vue'),
+            path: '/admin/discographies',
+            name: 'Discographies',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
+            component: () => import('../Pages/Vtubers/DiscographiesPage.vue'),
+        },
+        {
+            path: '/admin/galleries',
+            name: 'galleries',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
+            component: () => import('../Pages/Vtubers/GalleriesPage.vue'),
         },
 
-
+        {
+            path: '/admin/events',
+            name: 'events',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
+            component: () => import('../Pages/Vtubers/EventsPage.vue'),
+        },
         // Settings
         {
-            path: '/settings',
+            path: '/admin/settings',
             name: 'settings',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/Settings/SettingPage.vue'),
         },
         {
-            path: '/tabs',
+            path: '/admin/tabs',
             name: 'tabs',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/TabsPage.vue'),
         },
-        
-
         // Elements
-
         {
-            path: '/elements/buttons-standard',
+            path: '/admin/elements/buttons-standard',
             name: 'buttons-standard',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Elements/Buttons/StandardComponent.vue'),
         },
         {
-            path: '/elements/dropdowns',
-            name: 'dropdowns',
-            component: () => import('../DemoPages/Elements/DropdownsComponent.vue'),
-        },
-        {
-            path: '/elements/icons',
+            path: '/admin/elements/icons',
             name: 'icons',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Elements/IconsComponent.vue'),
         },
         {
-            path: '/elements/badges-labels',
+            path: '/admin/elements/badges-labels',
             name: 'badges',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Elements/BadgesComponent.vue'),
         },
         {
-            path: '/elements/cards',
+            path: '/admin/elements/cards',
             name: 'cards',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Elements/CardsComponent.vue'),
         },
         {
-            path: '/elements/list-group',
+            path: '/admin/elements/list-group',
             name: 'list-group',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Elements/ListGroupsComponent.vue'),
         },
         {
-            path: '/elements/timelines',
+            path: '/admin/elements/timelines',
             name: 'timeline',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Elements/TimelineComponent.vue'),
         },
         {
-            path: '/elements/utilities',
+            path: '/admin/elements/utilities',
             name: 'utilities',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Elements/UtilitiesComponent.vue'),
         },
-
         // Components
-
         {
-            path: '/components/tabs',
-            name: 'tabs',
-            component: () => import('../DemoPages/Components/TabsComponent.vue'),
-        },
-        {
-            path: '/components/accordions',
+            path: '/admin/components/accordions',
             name: 'accordions',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Components/AccordionsComponent.vue'),
         },
         {
-            path: '/components/modals',
+            path: '/admin/components/modals',
             name: 'modals',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Components/ModalsComponent.vue'),
         },
         {
-            path: '/components/progress-bar',
+            path: '/admin/components/progress-bar',
             name: 'progress-bar',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Components/ProgressBarComponent.vue'),
         },
         {
-            path: '/components/tooltips-popovers',
+            path: '/admin/components/tooltips-popovers',
             name: 'tooltips-popovers',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Components/TooltipsPopoversComponent.vue'),
         },
         {
-            path: '/components/carousel',
+            path: '/admin/components/carousel',
             name: 'carousel',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Components/CarouselComponent.vue'),
         },
         {
-            path: '/components/pagination',
-            name: 'pagination',
-            component: () => import('../DemoPages/Components/PaginationPage.vue'),
-        },
-        {
-            path: '/components/maps',
+            path: '/admin/components/maps',
             name: 'maps',
             component: () => import('../DemoPages/Components/MapsComponent.vue'),
         },
-
         // Tables
-
         {
-            path: '/tables/regular-tables',
+            path: '/admin/tables/regular-tables',
             name: 'regular-tables',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Tables/RegularTables.vue'),
         },
-
         // Dashboard Widgets
-
         {
-            path: '/widgets/chart-boxes-3',
+            path: '/admin/widgets/chart-boxes-3',
             name: 'chart-boxes-3',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Widgets/ChartBoxes3.vue'),
         },
-
         // Forms
-
         {
-            path: '/forms/controls',
+            path: '/admin/forms/controls',
             name: 'controls',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Forms/Elements/ControlsComponent.vue'),
         },
         {
-            path: '/forms/layouts',
+            path: '/admin/forms/layouts',
             name: 'layouts',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/Forms/Elements/LayoutsComponent.vue'),
         },
-
         // Kanban
-
         {
-            path: '/kanban',
+            path: '/admin/kanban',
             name: 'kanban',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../Pages/Kanban/KanbanComponent.vue'),
         },
-
         // Chatbox
-
         {
-            path: '/chatbox',
+            path: '/admin/chatbox',
             name: 'chatbox',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/ChatBox/ChatBox.vue'),
         },
-
         {
-            path: '/chatgroup',
+            path: '/admin/chatgroup',
             name: 'chatgroup',
+            meta: {
+                layout: 'admin',
+                requiresAuth: true
+            },
             component: () => import('../DemoPages/ChatBox/ChatGroup.vue'),
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('../DemoPages/UserPages/LoginBoxed.vue'),
+        },
+        // Pages
+        {
+            path: '/pages/login',
+            name: 'login',
+            component: () => import('../DemoPages/UserPages/LoginBoxed.vue'),
+        },
+        {
+            path: '/pages/register',
+            name: 'register',
+            meta: {
+                layout: 'userpages',
+            },
+            component: () => import('../DemoPages/UserPages/RegisterBoxed.vue'),
+        },
+        {
+            path: '/pages/forgot-password',
+            name: 'forgot-password',
+            meta: {
+                layout: 'userpages',
+            },
+            component: () => import('../DemoPages/UserPages/ForgotPasswordBoxed.vue'),
+        },
+        {
+            path: '*',
+            name: 'not-found',
+            meta: {
+                layout: 'userpages',
+            },
+            component: () => import('../Pages/NotFoundPage.vue'),
         },
     ]
 })
+
+// router.beforeEach(async (to, from, next) => {
+//     const { data: { user } } = await supabase.auth.getUser();
+//     const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+//     if(requiresAuth && !user) {
+//         next('/login');
+//         console.log("login")
+//     } else if(!requiresAuth && user) {
+//         next("/admin");
+//         console.log("admin")
+//     } else {
+//         console.log("in")
+//         next();
+//     }
+//   })
+export default router;
