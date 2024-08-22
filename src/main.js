3768d9c3150d9b9b6e3fde3e7e8fda0a041163d6
Joshua Trịnh <joshuatrinh5262@gmail.com>
2024-08-22 23:12:08 +0700

    remove bootstrap and add admin layout in main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
 import Vue from 'vue';
 import router from './router';
 import store from './store/store';
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 
 import App from './App';
 
+import Admin from './Layout/Wrappers/adminLayout.vue';
 import Default from './Layout/Wrappers/baseLayout.vue';
 import Pages from './Layout/Wrappers/pagesLayout.vue';
 
 Vue.config.productionTip = false;
 
+Vue.component('admin-layout', Admin);
 Vue.component('default-layout', Default);
 Vue.component('userpages-layout', Pages);
 
