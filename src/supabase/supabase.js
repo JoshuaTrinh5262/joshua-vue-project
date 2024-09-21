c9aa40ded24af03bc9d4aa8e14f5705ffc36911f
Joshua Trịnh <joshuatrinh5262@gmail.com>
2024-09-22 02:02:24 +0700

    update supabase

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://wtogyxxhemugevdrilkc.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0b2d5eHhoZW11Z2V2ZHJpbGtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk1NDM0NTgsImV4cCI6MjAzNTExOTQ1OH0.E8R9RJ-HAR7bkj0EhnXD_RKu8rNwwFH4sfG1D6luSzo";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = process.env.VUE_APP_SUPABASE_URL;
+const supabaseKey = process.env.VUE_APP_SUPABASE_KEY;
+const serviceRoleKey = process.env.VUE_APP_SERVICE_ROLE_KEY;
+export const supabase = createClient(supabaseUrl, serviceRoleKey);
 
 // supabase.auth.onAuthStateChange((event, session) => {
 //     if (event === 'SIGNED_IN') {
