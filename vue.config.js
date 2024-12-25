322db8cb127d842ae2c3c3adc2b5306f776191e4
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2024-12-25 10:00:52 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -15,21 +15,4 @@ module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
     ? './' // Production base path
     : '/', // Development base path
-
-  chainWebpack: config => {
-    config.resolve.alias.set('vue', '@vue/compat');
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2,
-            },
-          },
-        };
-      });
-  },
 });
