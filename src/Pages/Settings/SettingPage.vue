3f10f02f0e29eb8dc7c55557f4069b072490996a
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2024-12-27 16:30:54 +0700

    Update SettingPage.vue

--- src/Pages/Settings/SettingPage.vue
+++ src/Pages/Settings/SettingPage.vue
@@ -23,6 +23,18 @@
                     <button @click="changeLanguage" class="btn btn-primary"> {{ $t('apply_setting') }}</button>
                 </div>
                 <div class="divider"></div>
+                <!-- Darkmode Section -->
+                <div class="position-relative row form-group">
+                    <label for="darkmode" class="col-sm-2 col-form-label">Dark mode</label>
+                    <div class="col-sm-10">
+                        <button id="darkmode" class="btn btn-primary" @click="toggleDark()">
+                            <i v-if="!isDark" class="pe-7s-sun"></i>
+                            <i v-else class="pe-7s-moon"></i>
+                            Toggle Dark Mode
+                        </button>
+                    </div>
+                </div>
+                <div class="divider"></div>
                 <!-- Other Selection -->
                 <div class="position-relative row form-group">
                     <label class="col-sm-2 col-form-label">Demo Date Time Picker</label>
@@ -45,6 +57,7 @@ import { useI18n } from 'vue-i18n';
 import PageTitleComponent from "../../Layout/Components/PageTitleComponent.vue";
 import NotificationComponent from "../../Layout/Components/NotificationComponent.vue";
 import DateTimePicker from "../../DemoPages/Components/DateTimePicker.vue";
+import { useDark, useToggle } from "@vueuse/core";
 
 export default defineComponent({
     name: "SettingPage",
@@ -56,6 +69,8 @@ export default defineComponent({
     },
 
     setup() {
+        const isDark = useDark();
+        const toggleDark = useToggle(isDark);
         const { t, locale } = useI18n();
         const heading = ref(t("settings"));
         const subheading = ref("Setting Page.");
@@ -87,6 +102,7 @@ export default defineComponent({
             notification,
             selectedLanguage,
             changeLanguage,
+            toggleDark,
         };
     },
 });
