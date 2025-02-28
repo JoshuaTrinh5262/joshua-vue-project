577527a56950cbaca9a1f62aca6f3f52cc3d3d93
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2025-02-28 15:43:02 +0700

    Create yugiohBanlistApi.js

---
+++ src/supabase/api/yugiohBanlistApi.js
@@ -0,0 +1,128 @@
+import { supabase } from "../supabase";
+export const getYugiohBanlistsWithPaging = async (
+    page,
+    pageSize,
+    orderBy,
+    orderDirection,
+    search = ""
+) => {
+    try {
+        const start = (page - 1) * pageSize;
+        const end = start + pageSize - 1;
+
+        let query = supabase
+            .from("yugioh_banlist")
+            .select("*", {
+                count: "exact",
+            })
+            .order(orderBy, { ascending: orderDirection === "asc" })
+            .range(start, end);
+
+        if (search) {
+            query = query.or(`name.ilike.%${search}%`);
+        }
+
+        const { data, count, error } = await query;
+
+        if (error) {
+            throw error;
+        }
+
+        return {
+            items: data,
+            totalItems: count,
+            totalPages: Math.ceil(count / pageSize),
+        };
+    } catch (err) {
+        return { error: err.message };
+    }
+};
+
+export const getYugiohBanlists = async () => {
+    try {
+        const { data, error } = await supabase.from("yugioh_banlist").select("*");
+        if (error) {
+            throw error;
+        }
+        return data;
+    } catch (err) {
+        return { error: err.message };
+    }
+};
+
+export const getYugiohBanlistById = async (id) => {
+    try {
+        const { data, error } = await supabase
+            .from("yugioh_banlist")
+            .select("*")
+            .eq("id", id)
+            .single();
+        if (error) {
+            throw error;
+        }
+        return data;
+    } catch (err) {
+        return { error: err.message };
+    }
+};
+
+export const createYugiohBanlist = async (yugioh_banlist) => {
+    try {
+        const { data, error } = await supabase
+            .from("yugioh_banlist")
+            .insert(yugioh_banlist)
+            .single();
+        if (error) {
+            throw error;
+        }
+        return data;
+    } catch (err) {
+        return { error: err.message };
+    }
+};
+
+export const updateYugiohBanlist = async (update) => {
+    try {
+        const { data, error } = await supabase
+            .from("yugioh_banlist")
+            .update(update)
+            .eq("id", update.id)
+            .single();
+        if (error) {
+            throw error;
+        }
+        return data;
+    } catch (err) {
+        return { error: err.message };
+    }
+};
+
+export const deleteYugiohBanlist = async (id) => {
+    try {
+        const { data, error } = await supabase
+            .from("yugioh_banlist")
+            .delete()
+            .eq("id", id);
+        if (error) {
+            throw error;
+        }
+        return data;
+    } catch (err) {
+        return { error: err.message };
+    }
+};
+
+
+export const countYugiohBanlistRecord = async () => {
+    try {
+        const { count, error } = await supabase
+            .from("yugioh_banlist")
+            .select("*", { count: "exact", head: true });
+        if (error) {
+            throw error;
+        }
+        return count;
+    } catch (err) {
+        return { error: err.message };
+    }
+};
