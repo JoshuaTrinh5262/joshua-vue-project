8a4cf8f6ee42dd8e182cda778bb43dd051d4e89a
Joshua Trịnh <joshuatrinh5262@gmail.com>
2025-03-10 22:09:22 +0700

    adding dist subtree

---
+++ js/591.6fe29549.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkjoshua_vue_project"]=self["webpackChunkjoshua_vue_project"]||[]).push([[591],{1591:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-function formatString(str) {
-    if(str == null) {
-        return '';
-    }
-    return str.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-}
-
-export default formatString
