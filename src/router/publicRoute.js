03f6d2c4b80ae30ccd4494bf4364735c3b104b25
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2025-03-19 10:32:02 +0700

    Update publicRoute.js

--- src/router/publicRoute.js
+++ src/router/publicRoute.js
@@ -1,1 +1,32 @@
+const publicRoutes = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("../Public/HomePage.vue"),
+    },
+    {
+        path: "/yugioh/card",
+        name: "Public Card",
+        component: () => import("../Public/Yugioh/CardPublicPage.vue"),
+    },
+    {
+        path: "/yugioh/banlist",
+        name: "Public Banlist",
+        component: () => import("../Public/Yugioh/BanlistPublicPage.vue"),
+    },
+    {
+        path: "/yugioh/tournament",
+        name: "Public Tournament",
+        component: () => import("../Public/Yugioh/TournamentPublicPage.vue"),
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "not-found",
+        meta: {
+            layout: "userpages",
+        },
+        component: () => import("../Public/NotFoundPage.vue"),
+    },
+];
 
+export default publicRoutes;
