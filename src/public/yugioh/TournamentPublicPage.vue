efaae50e80cd3e3375ca8e63aecb6a44ad37e144
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2025-03-19 10:41:41 +0700

    Create TournamentPublicPage.vue

---
+++ src/public/yugioh/TournamentPublicPage.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <table-component
+      :footer="true"
+      :fields="fields"
+      :items="items"
+      @search="onSearch"
+    >
+    </table-component>
+
+    <pagination-component
+      :currentPage="currentPage"
+      :perPage="itemsPerPage"
+      :totalItems="totalItems"
+      :totalPages="totalPages"
+      @load-page="changeCurrentPage"
+      @change-page-size="changePageSize"
+    ></pagination-component>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+import PageTitleComponent from "../../Layout/Components/PageTitleComponent.vue";
+import TableComponent from "../../Layout/Components/TableComponent.vue";
+import PaginationComponent from "../../Layout/Components/PaginationComponent.vue";
+import ModalComponent from "../../Layout/Components/ModalComponent.vue";
+import ButtonSpinner from "../../Layout/Components/ButtonSpinner.vue";
+import { apiService } from "../../supabase/apiService";
+
+export default defineComponent({
+  name: "YugiohTournamentPublicPage",
+
+  components: {
+    PageTitleComponent,
+    TableComponent,
+    PaginationComponent,
+    ModalComponent,
+    ButtonSpinner,
+  },
+
+  setup() {
+    const currentPage = ref(1);
+    const itemsPerPage = ref(20);
+    const totalItems = ref(0);
+    const totalPages = ref(0);
+    const orderBy = ref("id");
+    const orderDirection = ref("asc");
+    const search = ref("");
+
+    const fields = ref([
+      {
+        key: "id",
+        value: "Id",
+      },
+      {
+        key: "name",
+        value: "Name",
+      },
+    ]);
+    const items = ref([]);
+
+    const onSearch = (newSearchTerm) => {
+      search.value = newSearchTerm;
+      getYugiohTournamentsData(1, itemsPerPage.value);
+    };
+
+    const getYugiohTournamentsData = async (newPage, newPageSize) => {
+      const result = await apiService.getYugiohTournamentsWithPaging(
+        newPage,
+        newPageSize,
+        orderBy.value,
+        orderDirection.value,
+        search.value
+      );
+      if (!result.error) {
+        items.value = result.items;
+        totalItems.value = result.totalItems;
+        totalPages.value = result.totalPages;
+        itemsPerPage.value = newPageSize;
+      }
+    };
+
+    const changePageSize = async (newPageSize) => {
+      await getYugiohTournamentsData(1, newPageSize);
+    };
+
+    const changeCurrentPage = async (newPage) => {
+      currentPage.value = newPage;
+      await getYugiohTournamentsData(currentPage.value, itemsPerPage.value);
+    };
+
+    onMounted(async () => {
+      await getYugiohTournamentsData(currentPage.value, itemsPerPage.value);
+    });
+
+    return {
+      currentPage,
+      itemsPerPage,
+      totalItems,
+      totalPages,
+      fields,
+      items,
+      onSearch,
+      changePageSize,
+      changeCurrentPage,
+    };
+  },
+});
+</script>
