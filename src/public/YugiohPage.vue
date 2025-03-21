539ea029c24610606d0b9dd6c73fe077a1e3eef0
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2025-03-21 16:25:22 +0700

    Create YugiohPage.vue

---
+++ src/public/YugiohPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <breadcrumb-component></breadcrumb-component>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import BreadcrumbComponent from "../Layout/Components/BreadcrumbComponent.vue";
+
+export default defineComponent({
+  name: "YugiohPage",
+  data() {
+    return {};
+  },
+  components: { BreadcrumbComponent },
+});
+</script>
