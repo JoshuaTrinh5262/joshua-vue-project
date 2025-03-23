466af017eeb0a4ff2c135f17c803cc02c11d2b62
Joshua Trịnh <joshuatrinh5262@gmail.com>
2025-03-23 23:54:08 +0700

    update

--- src/router/publicRoute.js
+++ src/router/publicRoute.js
@@ -3,43 +3,41 @@ const publicRoutes = [
         path: "/",
         name: "Home",
         meta: {
-            breadcrumb: 'Home'
+            breadcrumb: "Home",
         },
         component: () => import("../public/HomePage.vue"),
     },
     {
         path: "/yugioh",
-        name: "Yugioh",
-        meta: {
-            breadcrumb: 'Yugioh'
-        },
+        name: "yugioh",
         component: () => import("../public/YugiohPage.vue"),
-        children: [
-            {
-                path: 'card',
-                name: 'Card',
-                component: () => import("../public/yugioh/CardPublicPage.vue"),
-                meta: {
-                    breadcrumb: 'Cards'
-                }
-            },
-            {
-                path: 'banlist',
-                name: 'Banlist',
-                component: () => import("../public/yugioh/BanlistPublicPage.vue"),
-                meta: {
-                    breadcrumb: 'Banlist'
-                }
-            },
-            {
-                path: "tournament",
-                name: "Tournament",
-                meta: {
-                    breadcrumb: 'Tournament'
-                },
-                component: () => import("../public/yugioh/TournamentPublicPage.vue"),
-            },
-        ],
+        meta: {
+            breadcrumb: "yugioh",
+        },
+    },
+    {
+        path: "/yugioh/card",
+        name: "Card",
+        component: () => import("../public/yugioh/CardPublicPage.vue"),
+        meta: {
+            breadcrumb: "Cards",
+        },
+    },
+    {
+        path: "/yugioh/banlist",
+        name: "Banlist",
+        component: () => import("../public/yugioh/BanlistPublicPage.vue"),
+        meta: {
+            breadcrumb: "Banlist",
+        },
+    },
+    {
+        path: "/yugioh/tournament",
+        name: "Tournament",
+        meta: {
+            breadcrumb: "Tournament",
+        },
+        component: () => import("../public/yugioh/TournamentPublicPage.vue"),
     },
     {
         path: "/:pathMatch(.*)*",
