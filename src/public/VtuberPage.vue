0809e9670f3f11be312d80d8fd4b6e07a04cbddc
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2025-03-26 16:40:57 +0700

    Create VtuberPage.vue

---
+++ src/public/VtuberPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <breadcrumb-component></breadcrumb-component>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import BreadcrumbComponent from "../Layout/Components/BreadcrumbComponent.vue";
+
+export default defineComponent({
+  name: "YugiohPage",
+  setup() {
+    return {};
+  },
+  components: { BreadcrumbComponent },
+});
+</script>
