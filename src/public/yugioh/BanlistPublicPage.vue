ee499b829bc9a4c918bb46f1e1088c9b4b2f1b62
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2025-03-26 17:30:56 +0700

    Update BanlistPublicPage.vue

--- src/public/yugioh/BanlistPublicPage.vue
+++ src/public/yugioh/BanlistPublicPage.vue
@@ -1,9 +1,67 @@
 <template>
-  <div>
-    <breadcrumb-component></breadcrumb-component>
-
-    <div v-for="(item, index) in items" :key="index">
-      {{ item.id }} {{ item.name }}
+  <breadcrumb-component></breadcrumb-component>
+  <div class="card">
+    <div class="card-body">
+      <h3 class="card-title" for="selected_banlist">Selected Banlist</h3>
+      <div class="form-inline">
+        <div class="position-relative form-group">
+          <select
+            id="selected_banlist"
+            v-model="selectedBanlist"
+            class="form-control mr-2"
+          >
+            <option
+              v-for="(banlist, index) in banlists"
+              :key="index"
+              :value="banlist.id"
+            >
+              {{ banlist.name }}
+            </option>
+          </select>
+        </div>
+        <button-spinner :isLoading="onSubmit" buttonClass="btn btn-primary" @click="getYugiohBanlistDetailData"
+        :normalText="Submit" />
+      </div>
+    </div>
+    <div v-if="banlistDetail.length == 0" class="card-body">
+      Banlist is empty
+    </div>
+    <div
+      v-else
+      class="card-body"
+      v-for="(detail, index) in banlistDetail"
+      :key="index"
+    >
+      <h3 class="card-title">{{ index }} ({{ detail.length }})</h3>
+      <ul class="list-group list-group-flush">
+        <li
+          class="list-group-item"
+          v-for="(card, index) in detail"
+          :key="index"
+        >
+          <div class="widget-content p-0">
+            <div class="widget-content-wrapper">
+              <div class="widget-content-left mr-3">
+                <img
+                  width="40"
+                  height="60"
+                  :src="'/photo-coming-soon-holder.png'"
+                  :alt="card.yugioh_card?.name"
+                />
+              </div>
+              <div class="widget-content-left" style="min-width: 250px">
+                <div class="widget-heading">{{ card.yugioh_card?.name }}</div>
+                <div class="widget-subheading">
+                  {{ card.yugioh_card?.category }}
+                </div>
+              </div>
+              <div class="widget-content-left">
+                <p>{{ card.yugioh_card?.description }}</p>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -11,8 +69,6 @@
 <script>
 import { defineComponent, ref, onMounted } from "vue";
 import BreadcrumbComponent from "../../Layout/Components/BreadcrumbComponent.vue";
-import TableComponent from "../../Layout/Components/TableComponent.vue";
-import PaginationComponent from "../../Layout/Components/PaginationComponent.vue";
 import ModalComponent from "../../Layout/Components/ModalComponent.vue";
 import ButtonSpinner from "../../Layout/Components/ButtonSpinner.vue";
 import { apiService } from "../../supabase/apiService";
@@ -22,81 +78,40 @@ export default defineComponent({
 
   components: {
     BreadcrumbComponent,
-    TableComponent,
-    PaginationComponent,
     ModalComponent,
     ButtonSpinner,
   },
 
   setup() {
-    const currentPage = ref(1);
-    const itemsPerPage = ref(20);
-    const totalItems = ref(0);
-    const totalPages = ref(0);
-    const orderBy = ref("id");
-    const orderDirection = ref("asc");
-    const search = ref("");
+    const selectedBanlist = ref(1);
+    const banlists = ref([]);
+    const banlistDetail = ref([]);
 
-    const fields = ref([
-      {
-        key: "id",
-        value: "Id",
-      },
-      {
-        key: "name",
-        value: "Name",
-      },
-      {
-        key: "effective_date",
-        value: "Effective Date",
-      },
-    ]);
-    const items = ref([]);
-
-    const onSearch = (newSearchTerm) => {
-      search.value = newSearchTerm;
-      getYugiohBanlistsData(1, itemsPerPage.value);
-    };
-
-    const getYugiohBanlistsData = async (newPage, newPageSize) => {
-      const result = await apiService.getYugiohBanlistsWithPaging(
-        newPage,
-        newPageSize,
-        orderBy.value,
-        orderDirection.value,
-        search.value
-      );
+    const getYugiohBanlistData = async () => {
+      const result = await apiService.getYugiohBanlists();
       if (!result.error) {
-        items.value = result.items;
-        totalItems.value = result.totalItems;
-        totalPages.value = result.totalPages;
-        itemsPerPage.value = newPageSize;
+        banlists.value = result;
       }
     };
 
-    const changePageSize = async (newPageSize) => {
-      await getYugiohBanlistsData(1, newPageSize);
+    const getYugiohBanlistDetailData = async () => {
+      const result = await apiService.getYugiohBanlistById(
+        selectedBanlist.value
+      );
+      if (!result.error) {
+        console.log(result);
+        banlistDetail.value = result;
+      }
     };
-
-    const changeCurrentPage = async (newPage) => {
-      currentPage.value = newPage;
-      await getYugiohBanlistsData(currentPage.value, itemsPerPage.value);
-    };
-
     onMounted(async () => {
-      await getYugiohBanlistsData(currentPage.value, itemsPerPage.value);
+      await getYugiohBanlistData();
     });
 
     return {
-      currentPage,
-      itemsPerPage,
-      totalItems,
-      totalPages,
-      fields,
-      items,
-      onSearch,
-      changePageSize,
-      changeCurrentPage,
+      selectedBanlist,
+      banlists,
+      banlistDetail,
+      getYugiohBanlistDetailData,
     };
   },
 });
