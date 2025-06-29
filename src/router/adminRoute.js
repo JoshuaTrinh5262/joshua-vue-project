d6b4cdd5bf57e72d7be91a5bda5fe32ed3d9b5a8
Joshua Trịnh <joshuatrinh5262@gmail.com>
2025-06-29 14:05:55 +0700

    disable unuse route

--- src/router/adminRoute.js
+++ src/router/adminRoute.js
@@ -217,16 +217,16 @@ const adminRoutes = [
         },
         component: () => import("../Pages/Yugioh/DeckPage.vue"),
     },
-    {
-        path: "/admin/deck/:id",
-        name: "deck detail",
-        meta: {
-            layout: "admin",
-            requiresAuth: true,
-        },
-        props: true,
-        component: () => import("../Pages/Yugioh/Detail/DeckDetailPage.vue"),
-    },
+    // {
+    //     path: "/admin/deck/:id",
+    //     name: "deck detail",
+    //     meta: {
+    //         layout: "admin",
+    //         requiresAuth: true,
+    //     },
+    //     props: true,
+    //     component: () => import("../Pages/Yugioh/Detail/DeckDetailPage.vue"),
+    // },
     {
         path: "/admin/duelist",
         name: "duelist",
